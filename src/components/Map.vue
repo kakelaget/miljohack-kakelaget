c688d64fbd873beaac1ab5a1d9c4a3f4c2cd77cb
Kevin Midbøe <kevin.midboe@gmail.com>
2019-02-07 11:10:45 +0100

    I will show you the world 🗺

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,22 +1,37 @@
 <template>
-	<div>
-		<h1>Maaap</h1>
-	  <MglMap :accessToken="accessToken" :mapStyle.sync="mapStyle" />
-	 </div>
+  <div>
+  	<h1>Maaap</h1>
+	  <mapbox :access-token="accessToken" :map-options="options">
+  </mapbox>
+  </div>
 </template>
 
 <script>
-import { MglMap } from "vue-mapbox";
+import Mapbox from 'mapbox-gl-vue';
 
 export default {
   components: {
-    MglMap
+     'mapbox': Mapbox
   },
   data() {
-    return {
-      accessToken: "pk.eyJ1Ijoia2FzcGVycnQiLCJhIjoiY2pydWVyaHl1MDN6djQ0bDVuc3ZseDIzeCJ9.1r8QxHcJboxo3PJArWKOYQ", // your access token. Needed if you using Mapbox maps
-	    mapStyle: "mapbox://styles/kasperrt/cjrufjc6x0gbo1fp36xs6hamh",
-	  }
+  	return {
+  		accessToken: "pk.eyJ1Ijoia2FzcGVycnQiLCJhIjoiY2pydWVyaHl1MDN6djQ0bDVuc3ZseDIzeCJ9.1r8QxHcJboxo3PJArWKOYQ",
+  		options: {
+	      style: 'mapbox://styles/kasperrt/cjrufjc6x0gbo1fp36xs6hamh',
+  	    center: [10.7601723, 59.9167327],
+    	  zoom: 16,
+    	}
+  	}
   }
-};
+}
 </script>
+
+<style>
+	#map {
+		width: 100%;
+		height: 80vh;
+		position: absolute;
+		margin:0;
+		z-index:1;
+	}
+</style>
